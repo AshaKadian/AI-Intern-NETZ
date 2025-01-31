88a5b449d3817a3f64a2a734c8338e6585edea0f
Asha Kadian <99858828+AshaKadian@users.noreply.github.com>
2025-01-31 11:41:24 +0530

    Add files via upload

---
+++ pyhton Netz/advanced python/file_handling.py
@@ -0,0 +1,17 @@
+#create and write into the file
+# f= open("ashakad.txt",'a')
+# f.write("My name is Asha kad")
+# f.close()
+
+#read file
+# f= open("ashakad.txt",'')
+# print(f.read())
+# f.close()
+
+
+#delete existing file
+# import os
+# if os.path.exists("ashakad.txt"):
+#   os.remove("ashakad.txt")
+# else:
+#   print("The file does not exist")
